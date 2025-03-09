51631195e65fea145a8fe94c6da7f6e26891ec12
Joaquin <joaquin.romo@gmail.com>
2025-03-09 00:32:24 -0800

    test domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://peanutbutterandjelly.ai',
+	site: 'https://j-romo.github.io',
 	integrations: [mdx(), sitemap()],
 });
