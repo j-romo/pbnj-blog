5edf2a8a8e48c260b166ccd2f2db451597d6a61e
Joaquin <joaquin.romo@gmail.com>
2025-03-09 21:49:20 -0700

    remove custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://j-romo.github.io',
+	//site: 'https://www.peanutbutterandjelly.ai',
+	base: '/pbnj-blog',
 	integrations: [mdx(), sitemap()],
 });
